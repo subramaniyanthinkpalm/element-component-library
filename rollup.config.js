7e6288dc2a4681d547bef2924f63ea766d773279
subramaniyan.t <subramaniyan.t@thinkpalm.com>
2021-03-03 18:10:09 +0530

    Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,12 @@ import external from 'rollup-plugin-peer-deps-external';
 import del from 'rollup-plugin-delete';
 import pkg from './package.json';
 import postcss from 'rollup-plugin-postcss';
-import path from 'path'
+import path from 'path';
+import simplevars from 'postcss-simple-vars';
+import nested from 'postcss-nested';
+import cssnext from 'postcss-cssnext';
+import cssnano from 'cssnano';
+import eslint from 'rollup-plugin-eslint';
 
 export default {
     input: pkg.source,
@@ -15,11 +20,24 @@ export default {
     plugins: [
         external(),
         postcss({
-            modules: true,
-            minimize: true,
+
+            plugins: [
+                simplevars(),
+                nested(),
+                cssnext({ warnForDuplicates: false, }),
+                cssnano(),
+            ],
             extensions: ['.css','.less'],
+            // modules: true,
+            // minimize: true,
+            // extensions: ['.css','.less'],
             // extract: path.resolve('dist/index-element.css'),
         }),
+        // eslint({
+        //     exclude: [
+        //       'src/styles/**',
+        //     ]
+        // }),
         babel({
             exclude: 'node_modules/**'
         }),
