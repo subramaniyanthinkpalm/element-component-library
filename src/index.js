5e72c8e3dd8a623316ce6ec0fb6adb2b20305adc
subramaniyan.t <subramaniyan.t@thinkpalm.com>
2021-03-03 21:38:29 +0530

    Semantic css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './semantic_ui.less';
-import './semantic.min.css';
+// import './semantic.min.css';
+import 'semantic-ui-css/semantic.min.css'
 export { default as HelloWorld } from './components/HelloWorld';
 export { default as Button } from './components/Button';
 export { default as Dropdown } from './components/Dropdown';
